f122a87ce6726fa4eceed99ac69353272aced4a6
jayanth2341 <yjayanth769@gmail.com>
2024-10-15 10:08:42 +0530

    Update Total run's of batsman.py

--- Total run's of batsman.py
+++ Total run's of batsman.py
@@ -1,5 +1,5 @@
 """
-4)
+
 Extras
 Extras are runs scored by a means other than a batsman hitting the ball. A batsman is not given credit for extras other than runs scored off the bat from a no ball, and the extras are tallied separately on the scorecard and count only towards the team’s score. the types of extras are No ball, Wide, Bye, Leg-bye and Penalty. 1 Penalty corresponds to 5 runs.
 Find the total runs that the Extras contribute to the team’s score, given the number of No-balls, wides, byes, leg-byes and penalty given off by the bowlers in innings.
